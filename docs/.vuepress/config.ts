03053343ca7bf4152889982d300281e02d2b2231
abtd <abaotiandi1205@outlook.com>
2021-10-01 17:31:04 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-CN',
+  title: '高空舰队中文手册',
+  description: '高空舰队中文手册'
+})
