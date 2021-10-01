e0b53d21582e03314970fc5f366e32fcd74412d8
abtd <abaotiandi1205@outlook.com>
2021-10-01 21:32:23 +0800

    github pages 配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,5 +4,6 @@ import type { DefaultThemeOptions } from 'vuepress'
 export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: '高空舰队中文手册',
-  description: '高空舰队中文手册'
+  description: '高空舰队中文手册',
+  base: '/highfleet-manual-cn/'
 })
